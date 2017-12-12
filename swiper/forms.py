f6e1ef43acdd17299337f25efc418b698ecc0a4e
Gavin Fynbo <gavfynbo@gmail.com>
2017-12-12 16:18:22 -0500

    add working matches and update matches page to show all results

--- swiper/forms.py
+++ swiper/forms.py
@@ -17,25 +17,3 @@ class MatchRestaurantForm(forms.Form):
         curr_user = User.objects.get(pk=user.pk)
         restaurant = Restaurant.objects.get(pk=self.cleaned_data['restaurant_id'])
         MatchManager.add_match(user=curr_user, restaurant=restaurant)
-
-    #  # Overriding save allows us to process the value of 'toppings' field
-    # def save(self, commit=True):
-    #     # Get the unsave Pizza instance
-    #     instance = forms.ModelForm.save(self, False)
-    #
-    #     # Prepare a 'save_m2m' method for the form,
-    #     old_save_m2m = self.save_m2m
-    #     def save_m2m():
-    #        old_save_m2m()
-    #        # This is where we actually link the pizza with toppings
-    #        instance.topping_set.clear()
-    #        for topping in self.cleaned_data['toppings']:
-    #            instance.topping_set.add(topping)
-    #     self.save_m2m = save_m2m
-    #
-    #     # Do we need to save all changes now?
-    #     if commit:
-    #         instance.save()
-    #         self.save_m2m()
-    #
-    #     return instance
